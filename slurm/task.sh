593f10016cfbe317c43ee7ffc6939e676b84b56d
Poloudenny <poloudenny@gmail.com>
2025-10-19 16:49:15 +0200

    .

--- slurm/task.sh
+++ slurm/task.sh
@@ -1,1 +1,1 @@
-python bin/eval/conditioning_decoding_sweep.py   --model-ckpt runs/deCIFer_cifs_v1_model/ckpt_eval.pt --dataset-path ../crystallography/data/structures/cifs_v1/serialized/test.h5 --out-root runs/deCIFer_cifs_v1_model/conditioning_decoding_sweep_100 --max-samples 100
+python bin/eval/conditioning_decoding_sweep.py   --model-ckpt runs/deCIFer_cifs_v1_model/ckpt_eval.pt --dataset-path ../crystallography/data/structures/cifs_v1/serialized/test.h5 --out-root runs/deCIFer_cifs_v1_model/conditioning_decoding_sweep_100 
