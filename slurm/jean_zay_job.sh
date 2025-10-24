64e6bb4988c40c05eb425be4da7e96aced520674
Poloudenny <poloudenny@gmail.com>
2025-10-24 10:05:49 +0200

    .

--- slurm/jean_zay_job.sh
+++ slurm/jean_zay_job.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
-#SBATCH --job-name=task
+#SBATCH --job-name=train_u_decifer
 #SBATCH --partition=gpu_p6
 #SBATCH --constraint=h100
 #SBATCH --gres=gpu:2
 #SBATCH --account=nxk@h100
 #SBATCH --time=12:00:00
-#SBATCH --output=logs/task.out
+#SBATCH --output=logs/train_u_decifer.out
 #SBATCH --ntasks-per-node=2
 #SBATCH --hint=nomultithread
+#SBATCH --cpus-per-task=32
 
 
 set -euo pipefail
 
 REPO_DIR='/home/gpoloudenny/Projects/deCIFer'
-COMMIT_HASH='2c2ce0228ebfabbc5f8b5d98b1060b9761e7e9b1'
-ORIGINAL_REF='beam_stoch'
-RUN_COMMAND='python bin/eval/beam_vs_rwp_filter.py  --model-ckpt runs/deCIFer_cifs_v1_model/ckpt_eval.pt --dataset-path ../crystallography/data/structures/cifs_v1/serialized/test.h5 --out-root runs/deCIFer_cifs_v1_model/beam_search_vs_rwp_filter  --max-samples 1000'
-GENERATED_AT='20251019_123329'
+COMMIT_HASH='3712400055e8108b2d8209a95c7c29f933aba2c5'
+ORIGINAL_REF='grpo'
+RUN_COMMAND='torchrun --nproc_per_node=2 bin/train.py --config configs/U-deCIFer_NOMA_training_config.yaml'
+GENERATED_AT='20251024_095904'
 
 mkdir -p "$WORK/deCIFer/logs"
 
 cd "$REPO_DIR"
 echo "[Jean Zay helper] Restoring commit $COMMIT_HASH"
 
-echo "[Jean Zay helper] Using modules: arch/h100 pytorch-gpu/py3/2.5.0 git"
+echo "[Jean Zay helper] Using modules: arch/h100 pytorch-gpu/py3/2.3.1 git"
 module purge
 module load arch/h100
-module load pytorch-gpu/py3/2.5.0
+module load pytorch-gpu/py3/2.3.1
 module load git
 
 git checkout $COMMIT_HASH
