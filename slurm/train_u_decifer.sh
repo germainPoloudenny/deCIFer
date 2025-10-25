77de9a8d8a9e8d1772d2c0a78a517d597908121d
Poloudenny <poloudenny@gmail.com>
2025-10-25 19:02:40 +0200

    .

--- slurm/train_u_decifer.sh
+++ slurm/train_u_decifer.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node=2 bin/train.py --config configs/U-deCIFer_NOMA_training_config.yaml
+torchrun --nproc_per_node=4 bin/train.py --config configs/U-deCIFer_NOMA_training_config.yaml
