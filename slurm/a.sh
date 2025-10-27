efeef2a40054463ae368cf85dd2198bca7d9581e
Poloudenny <poloudenny@gmail.com>
2025-10-27 10:58:45 +0100

    .

---
+++ slurm/a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=2#SBATCH --job-name=task
+#SBATCH --partition=gpu_p5
+#SBATCH --constraint=a100
+#SBATCH --gres=gpu:2
+#SBATCH --account=nxk@a100
+#SBATCH --time=12:00:00
+#SBATCH --output=logs/task.out
+#SBATCH --ntasks-per-node=2
+#SBATCH --hint=nomultithread
+#SBATCH --cpus-per-task=32
+
+
+set -euo pipefail
+
+REPO_DIR='/lustre/fswork/projects/rech/nxk/uvv78gt/deCIFer'
+
+cd "$REPO_DIR"
+
+module purge
+module load arch/a100
+module load pytorch-gpu/py3/2.3.0
+module load git
+
+git checkout grpo
+
+python bin/eval/conditioning_decoding_sweep.py   --model-ckpt runs/deCIFer_cifs_v1_model/ckpt_eval.pt --dataset-path ../crystallography/data/noma-1k/serialized/test.h5 --out-root runs/deCIFer_cifs_v1_model/conditioning_decoding_sweep --max-samples 1000 --collect-top-k-metric rmsd
---
+++ slurm/h.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=2#SBATCH --job-name=task
+#SBATCH --partition=gpu_p6
+#SBATCH --constraint=h100
+#SBATCH --gres=gpu:2
+#SBATCH --account=nxk@h100
+#SBATCH --time=12:00:00
+#SBATCH --output=logs/task.out
+#SBATCH --ntasks-per-node=2
+#SBATCH --hint=nomultithread
+#SBATCH --cpus-per-task=48
+
+
+set -euo pipefail
+
+REPO_DIR='/lustre/fswork/projects/rech/nxk/uvv78gt/deCIFer'
+
+cd "$REPO_DIR"
+
+module purge
+module load arch/h100
+module load pytorch-gpu/py3/2.3.1
+module load git
+
+git checkout grpo
+
+python bin/eval/conditioning_decoding_sweep.py   --model-ckpt runs/deCIFer_cifs_v1_model/ckpt_eval.pt --dataset-path ../crystallography/data/noma-1k/serialized/test.h5 --out-root runs/deCIFer_cifs_v1_model/conditioning_decoding_sweep --max-samples 1000 --collect-top-k-metric rmsd
